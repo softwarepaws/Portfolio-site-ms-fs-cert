16828922919bde0322ebac36f35c79e3fbaec099
Lin <limora96@gmail.com>
2025-12-19 23:00:45 -0800

    Add initial functions for menu and contact form

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+
+}
+
+function filterProjects(category) {
+
+}
+
+function hasContactFormEmail(email) {
+
+}
+
+function validateContactFormEmail(email) {
+
+}
+
+function hasContactFormName(name) {
+
+}
+
+function validateContactFormName(name) {
+
+}
+
+function hasContactFormMessage(message) {
+
+}
