36b4e04a8dad888baffd44bd4b42be1b3dab8a53
Lin <lin.mora@softwarepaws.com>
2026-04-04 12:19:47 -0700

    Fixed Google Adsense header banner

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./nav-bar/nav-bar";
 import { Footer } from "./footer/footer";
+import { Adsense } from "./adsense/adsense";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer],
+  imports: [RouterOutlet, NavBar, Footer, Adsense],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
