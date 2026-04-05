587f8e8242d1d02831df9d2798e64d5c146b46c2
Lin <lin.mora@softwarepaws.com>
2026-04-05 12:30:18 -0700

    Fixed navbar CSS and routing, added resume component

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+const bPoint600px = '(max-width: 600px)';
+
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgClass],
+  imports: [NgClass, RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
-  isMenuOpen: boolean = true;
+export class NavBar implements OnInit {
+  isMenuOpen = signal(true);
+
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([bPoint600px]).subscribe(result => {
+      const isMobile = result.matches;
+      
+      if (isMobile) {
+        this.isMenuOpen.set(false)
+      } else {
+        this.isMenuOpen.set(true)
+      }
+      
+      console.log("Is Mobile:", isMobile, "Menu Open:", this.isMenuOpen);
+    });
+  }
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+    this.isMenuOpen.update(current => !current);
   }
    
 }
